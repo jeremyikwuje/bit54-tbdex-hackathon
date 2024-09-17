1c933626c89aa10cecce37c75b58f30d0b6c71f9
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-09-17 17:55:15 +0100

    feat: update prototype link

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { error, redirect } from '@sveltejs/kit'
 
 export const load = async () => {
-	throw redirect(301, `https://www.figma.com/proto/1HWd5e5hahSpCZ7CdN1s8J/Bit54?page-id=0%3A1&node-id=241-437&node-type=canvas&viewport=275%2C191%2C0.12&t=EDeIU5AuGArE0XFD-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=14%3A618`);
+	throw redirect(301, `https://tinyurl.com/bit54-demo-link`);
 }
